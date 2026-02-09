8849b7e6555d0aaafa093d48f2124c5ecae0c686
Diego Riaño <diegoalejandroriano012@gmail.com>
2026-02-09 13:22:10 -0500

    corregi el boton de añadir por añadir super administrador

--- app/views/dashboard/superadministrador/usuarios.php
+++ app/views/dashboard/superadministrador/usuarios.php
@@ -38,7 +38,7 @@ include_once __DIR__ . '/../../layouts/header_superadministrador.php';
                                 ← Todos (<?= count($datos) ?>)
                             </button>
                         </div>
-                        <a href="<?= BASE_URL ?>/superadmin/registrar-usuario" class="btn btn-primary btn-sm" style="border-radius: 20px;"><i class="bi bi-plus-lg"></i> AÑADIR</a>
+                        <a href="<?= BASE_URL ?>/superadmin/registrar-usuario" class="btn btn-primary btn-sm" style="border-radius: 20px;"><i class="bi bi-plus-lg"></i> AÑADIR SUPER ADMINISTRADOR</a>
                     </div>
 
                     <!-- Consultorios Table -->
